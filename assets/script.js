f44d806afe093083573f043c0799326b9429357c
danieldebassige <danieldebassige@gmail.com>
2022-10-16 15:06:32 -0500

    Push and pray v2.2

--- assets/script.js
+++ assets/script.js
@@ -3,27 +3,25 @@ import { getAuth, signInWithPopup, GithubAuthProvider } from "https://www.gstati
 const auth = getAuth();
 const providerGithub = new GithubAuthProvider();
 
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    const credential = GithubAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GithubAuthProvider.credentialFromError(error);
-    // ...
-  });
-
 function ButtonClick() {
-    signInWithPopup(auth, providerGithub);
+    signInWithPopup(auth, providerGithub)
+    .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user)
+        // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+    });
 };
